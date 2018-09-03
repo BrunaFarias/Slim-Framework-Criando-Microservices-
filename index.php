918e0a9bedf0a97d6fd65604cded300dff2da117
Bruna Ferreira <bruna.ferreira@pmm.am.gov.br>
2018-09-03 14:58:14 -0400

    master

--- index.php
+++ index.php
@@ -1,69 +1,113 @@
 <?php
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
-
+use App\Models\Entity\Book;
 require 'bootstrap.php';
-
-/**$app = new \Slim\App;*/
-
 /**
- * Inicio do bang :)
- * @var string
+ * Lista de todos os livros
+ * @request curl -X GET http://localhost:8000/book
  */
-/**rota get por meio de uma Clojure */
-//exercicio 1
-/** $app->get('/', function (Request $request, Response $response) use ($app) {
-    $response->getBody()->write("Hello Word!");
-    return $response;
-});
-$app->run();*/
-
-/**GET /books — Listar todos os livros
-GET /books/id — Retorna informações de um livro em específico
-POST /books — Cria um novo livro
-PUT /books/id — Edita o post informado pelo id
-DELETE /books/id — Deleta o post informado pelo id */
-
-
-/**lista de todos o livros */
-
-$app->get('/book', function (Request $request, Response $response) use ($app){
-    $return = $response->withJson(['msg' => 'Lista de Livros'], 200);
+$app->get('/book', function (Request $request, Response $response) use ($app) {
+    $entityManager = $this->get('em');
+    $booksRepository = $entityManager->getRepository('App\Models\Entity\Book');
+    $books = $booksRepository->findAll();
+    $return = $response->withJson($books, 200)
+        ->withHeader('Content-type', 'application/json');
     return $return;
 });
-
-/**retornando mais informações do livro informado pelo id */
-
-$app->get('/book/{id}', function (Request $request, Response $response) use ($app){
+/**
+ * Retornando mais informações do livro informado pelo id
+ * @request curl -X GET http://localhost:8000/book/1
+ */
+$app->get('/book/{id}', function (Request $request, Response $response) use ($app) {
     $route = $request->getAttribute('route');
     $id = $route->getArgument('id');
-    $return = $response->withJson(['msg' => 'Exibindo livro'],200);
+    $entityManager = $this->get('em');
+    $booksRepository = $entityManager->getRepository('App\Models\Entity\Book');
+    $book = $booksRepository->find($id);        
+    $return = $response->withJson($book, 200)
+        ->withHeader('Content-type', 'application/json');
     return $return;
 });
-
-/**Cadastrando novo livro */
-
-$app->post('/book', function (Request $request, Response $response) use ($app){
-    $return = $response->withJson(['msg' => "Cadastrando um livro"], 201);
+/**
+ * Cadastra um novo <Livro></Livro>
+ * @request curl -X POST http://localhost:8000/book -H "Content-type: application/json" -d '{"name":"O Oceano no Fim do Caminho", "author":"Neil Gaiman"}'
+ */
+$app->post('/book', function (Request $request, Response $response) use ($app) {
+    $params = (object) $request->getParams();
+    /**
+     * Pega o Entity Manager do nosso Container
+     */
+    $entityManager = $this->get('em');
+    /**
+     * Instância da nossa Entidade preenchida com nossos parametros do post
+     */
+    $book = (new Book())->setName($params->name)
+        ->setAuthor($params->author);
+    
+    /**
+     * Persiste a entidade no banco de dados
+     */
+    $entityManager->persist($book);
+    $entityManager->flush();
+    $return = $response->withJson($book, 201)
+        ->withHeader('Content-type', 'application/json');
     return $return;
 });
-
-/**Atualiza os dados de um livro pelo ID */
-
-$app->put('/book/{id}', function (Request $request, Response $response) use ($app){
+/**
+ * Atualiza os dados de um livro
+ * @request curl -X PUT http://localhost:8000/book/14 -H "Content-type: application/json" -d '{"name":"Deuses Americanos", "author":"Neil Gaiman"}'
+ */
+$app->put('/book/{id}', function (Request $request, Response $response) use ($app) {
+    /**
+     * Pega o ID do livro informado na URL
+     */
     $route = $request->getAttribute('route');
     $id = $route->getArgument('id');
-    $return = $response->withJson(['msg' => "Atualizando dados {$id}"], 201);
+    /**
+     * Encontra o Livro no Banco
+     */ 
+    $entityManager = $this->get('em');
+    $booksRepository = $entityManager->getRepository('App\Models\Entity\Book');
+    $book = $booksRepository->find($id);   
+    /**
+     * Atualiza e Persiste o Livro com os parâmetros recebidos no request
+     */
+    $book->setName($request->getParam('name'))
+        ->setAuthor($request->getParam('author'));
+    /**
+     * Persiste a entidade no banco de dados
+     */
+    $entityManager->persist($book);
+    $entityManager->flush();        
+    
+    $return = $response->withJson($book, 200)
+        ->withHeader('Content-type', 'application/json');
     return $return;
 });
-
-/**Deleta um livro informado pelo ID */
-
-$app->delete('/book/{id}', function (Request $request, Response $response) use ($app){
+/**
+ * Deleta o livro informado pelo ID
+ * @request curl -X DELETE http://localhost:8000/book/3
+ */
+$app->delete('/book/{id}', function (Request $request, Response $response) use ($app) {
+    /**
+     * Pega o ID do livro informado na URL
+     */
     $route = $request->getAttribute('route');
     $id = $route->getArgument('id');
-    $return = $response->withJson(['msg' => "Deletando Livro {$id}"], 203);
+    /**
+     * Encontra o Livro no Banco
+     */ 
+    $entityManager = $this->get('em');
+    $booksRepository = $entityManager->getRepository('App\Models\Entity\Book');
+    $book = $booksRepository->find($id);   
+    /**
+     * Remove a entidade
+     */
+    $entityManager->remove($book);
+    $entityManager->flush(); 
+    $return = $response->withJson(['msg' => "Deletando o livro {$id}"], 204)
+        ->withHeader('Content-type', 'application/json');
     return $return;
 });
-
 $app->run();
--- src/Models/Entity/Book.php
+++ src/Models/Entity/Book.php
@@ -1,51 +1,54 @@
 <?php
-
-namespace App\Models\Enity;
-
-// @Entity @Table(name="books")
-
-
- class Book{
-     /**
+namespace App\Models\Entity;
+/**
+ * @Entity @Table(name="books")
+ **/
+class Book {
+    /**
      * @var int
      * @Id @Column(type="integer") 
      * @GeneratedValue
-         */
-    
-
+     */
     public $id;
-
-     /**
-      * @var string
-      *Column(type="string")
-      */
-
-    public $name;
-
     /**
-       * @var string
-       * @Column(type="string") 
-       */
+     * @var string
+     * @Column(type="string") 
+     */
+    public $name;
+    /**
+     * @var string
+     * @Column(type="string") 
+     */
     public $author;
-
+    /**
+     * @return int id
+     */
     public function getId(){
         return $this->id;
     }
-
+    /**
+     * @return string name
+     */
     public function getName(){
         return $this->name;
     }
-
-    public function getAuthor(){
+    /**
+     * @return string author
+     */
+    public function getAuthor() {
         return $this->author;
-    }
-
-    public function setName(){
+    }    
+    /**
+     * @return Book()
+     */
+    public function setName($name){
         $this->name = $name;
-        return $this;
+        return $this;  
     }
-
-    public function setAuthor(){
+     /**
+     * @return Book()
+     */
+    public function setAuthor($author) {
         $this->author = $author;
         return $this;
     }
